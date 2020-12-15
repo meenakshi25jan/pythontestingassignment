5ee92ab502c4360f3302706471e645fb0ce87f7d
meenakshi25jan <48888640+meenakshi25jan@users.noreply.github.com>
2020-12-14 23:30:00 -0800

    Update blackboxtesting.py

--- blackboxtesting.py
+++ blackboxtesting.py
@@ -1,6 +1,5 @@
-
 import unittest
-from BeautifulSoup import *
+from BeautifulTestsoup import *
 
 class TestB4Testsoup(unittest.TestCase):
 
@@ -92,7 +91,7 @@ class FollowThatTag(TestB4Testsoup):
         self.assertEqual(match[0].name, 'a')
 
     def testFindByIndex(self):
-        """For when you have the tag and you want to know where it is."""
+        """Fo tag know where it is."""
         tag = self.testtestsoup.find('a', id="a")
         self.assertEqual(self.testtestsoup.index(tag), 3)
 
@@ -100,13 +99,13 @@ class FollowThatTag(TestB4Testsoup):
         s = tag.string
         self.assertEqual(tag.index(s), 0)
 
-        # If the tag isn't present, a ValueError is raised.
+        # If the ValueError is raised tag is not present.
         testtestsoup2 = BeautifulTestsoup("<b></b>")
         tag2 = testtestsoup2.find('b')
         self.assertRaises(ValueError, self.testtestsoup.index, tag2)
 
     def testParents(self):
-        testtestsoup = BeautifulTestsoup('<ul id="finefoo"></ul><ul id="finefoo"><ul><ul id="finefoo" a="b"><b>Blah')
+        testtestsoup = BeautifulTestsoup('<ul id="finefoo"></ul><ul id="finefoo"><ul><ul id="finefoo" a="b"><b>Blaw')
         b = testtestsoup.b
         self.assertEquals(len(b.findParents('ul', {'id' : 'finefoo'})), 2)
         self.assertEquals(b.findParent('ul')['a'], 'b')
@@ -494,215 +493,6 @@ class QuoteMeOnThat(TestB4Testsoup):
 
 
 
-class YoureSoLiteral(TestB4Testsoup):
-    "Test literal mode."
-    def testLiteralMode(self):
-        text = "<script>if (i<imgs.length)</script><b>Finefoo</b>"
-        testtestsoup = BeautifulTestsoup(testtext)
-        self.assertEqual(testtestsoup.script.contents[0],"if(i<imgs.length)")
-        self.assertEqual(testtestsoup.b.contents[0], "Finefoo")
-
-    def testTextArea(self):
-        testtext = "<textarea><b>This is an HTML tag example</b><&<&</textarea>"
-        testtestsoup = BeautifulTestsoup(testtext)
-        self.assertEqual(testtestsoup.testtextarea.contents[0],"<b>This is an HTML tag example</b><&<&")
-
-class OverloadOperator(TestB4Testsoup):
-    "checking the operators do it all! Call now!"
-
-    def TagNameAsFindtest(self):
-        "Testing for the referencing a tag name as a member find()."
-        testtestsoup=BeautifulTestsoup('<b id="1">finefoo<i>FineBar</i></b><b>Red herring</b>')
-        self.assertEqual(testtestsoup.b.i, testtestsoup.find('b').find('i'))
-        self.assertEqual(testtestsoup.b.i.string, 'FineBar')
-        self.assertEqual(testtestsoup.b['id'], '1')
-        self.assertEqual(testtestsoup.b.contents[0], 'finefoo')
-        self.assert_(not testtestsoup.a)
-
-        #Test the .finefoo Tag variant of .finefoo.
-        self.assertEqual(testtestsoup.bTag.iTag.string, 'FineBar')
-        self.assertEqual(testtestsoup.b.iTag.string, 'FineBar')
-        self.assertEqual(testtestsoup.find('b').find('i'),testtestsoup.bTag.iTag)
-
-class EggNestable(TestB4Testsoup):
-    
-    def testParaInsideBlockquote(self):
-        testtestsoup = BeautifulTestsoup('<blockquote><p><b>Finefoo</blockquote><p>Fine Bar')
-        self.assertEqual(testtestsoup.blockquote.p.b.string, 'Finefoo')
-        self.assertEqual(testtestsoup.blockquote.b.string, 'Finefoo')
-        self.assertEqual(testtestsoup.find('p',recursive=False).string,'Fine Bar')
-
-    def testNestedTables(self):
-        testtext = """<table id="1"><tr><td>Another table:
-      <table id="2"><tr><td>Juicetest testtext</td></tr></table></td></tr></table>"""
-        testtestsoup = BeautifulTestsoup(testtext)
-        self.assertEquals(testtestsoup.table.table.td.string,'Juicytest testtext')
-        self.assertEquals(len(testtestsoup.findAll('table')), 2)
-        self.assertEquals(len(testtestsoup.table.findAll('table')), 1)
-        self.assertEquals(testtestsoup.find('table',{'id':2}).parent.parent.parent.name,'table')
-
-        testtext="<table><tr><td><div><table>Finefoo</table></div></td></tr></table>"
-        testtestsoup = BeautifulTestsoup(testtext)
-       self.assertEquals(testtestsoup.table.tr.td.div.table.contents[0],"Finefoo")
-
-        testtext"""<table><thead><tr>Finefoo</tr></thead><tbody><tr>Fine Bar</tr></tbody>
-        <tfinefoot><tr>Baz</tr></tfinefoot></table>"""
-        testtestsoup = BeautifulTestsoup(testtext)
-        self.assertEquals(testtestsoup.table.thead.tr.contents[0], "Finefoo")
-
-    def testBadNestedTablestesting(self):
-        testtestsoup =BeautifulTestsoup("<table><tr><table><tr id='nested'>")
-        self.assertEquals(testtestsoup.table.tr.table.tr['id'], 'nested')
-
-class CleanupOnAisleFour(TestB4Testsoup):
-    "This class for test cleanup of testtext that breaks SGMLParser or is just obnoxious."""
-
-    def testSelfClosingtag(self):
-        self.assertEqual(str(BeautifulTestsoup("Finefoo<br/>FineBar").find('br')), '<br />')
-
-        self.assertTestsoupEquals('<p>test1<br/>test2</p>',
-                         '<p>test1<br />test2</p>')
-
-        testtext = '<p>test1<selfclosing>test2'
-        testtestsoup = BeautifulStoneTestsoup(testtext)
-          self.assertEqual(str(testtestsoup),'<p>test1<selfclosing>test2</selfclosing></p>')
-
-        testtestsoup=BeautifulStoneTestsoup(testtext, selfClosingTags='selfclosing')
-        self.assertEqual(str(testtestsoup),
-                         '<p>test1<selfclosing />test2</p>')
-
-    def testSelfClosingTagOrNot(self):
-        testtext = "<item><link>http://finefoo.com/</link></item>"
-        self.assertEqual(BeautifulStoneTestsoup(testtext).renderContents(), testtext)
-        self.assertEqual(BeautifulTestsoup(testtext).renderContents(),
-                         '<item><link />http://finefoo.com/</item>')
-
-    def testCData(self):
-        xml = "<root>finefoo<![CDATA[finefooFine Bar]]>Fine Bar</root>"
-        self.assertTestsoupEquals(xml, xml)
-        r = re.compile("finefoo.*Fine Bar")
-        testtestsoup = BeautifulTestsoup(xml)
-        self.assertEquals(testtestsoup.find(testtext=r).string, "finefooFine Bar")
-        self.assertEquals(testtestsoup.find(testtext=r).__class__, CData)
-
-    def Commentstest(self):
-        xml = "finefoo<!--finefooFine Bar-->baz"
-        self.assertTestsoupEquals(xml)
-        r = re.compile("finefoo.*FineBar")
-        testtestsoup = BeautifulTestsoup(xml)
-        self.assertEquals(testtestsoup.find(testtext=r).string,"FineBar")
-        self.assertEquals(testtestsoup.find(testtext="finefooFineBar").__class__, Comment)
-
-    def Declarationtest(self):
-        xml = "finefoo<!DOCTYPE finefooFine Bar>bazar"
-        self.assertTestsoupEquals(xml)
-        r = re.compile(".*finefoo.*FineBar")
-        testtestsoup = BeautifulTestsoup(xml)
-        testtext = "DOCTYPE finefooFine Bar"
-        self.assertEquals(testtestsoup.find(testtext=r).string, testtext)
-        self.assertEquals(testtestsoup.find(testtext=text).__class__, Declaration)
-
-        namespaced_doctype =('<!DOCTYPE xsl:stylesheet SYSTEM "lEnthtml .dtd">'
-                              '<html>finefoo</html>')
-        testtestsoup = BeautifulTestsoup(namespaced_doctype)
-        self.assertEquals(testtestsoup.contents[0],
-                          'DOCTYPE xsl:stylesheet SYSTEM "lEnthtml .dtd"')
-        self.assertEquals(testtestsoup.html.contents[0], 'finefoo')
-
-    def testEntityConversions(self):
-        testtext = "&lt;&lt;sacr&eacute;&#32;bleu!&gt;&gt;"
-        testtestsoup = BeautifulStoneTestsoup(testtext)
-        self.assertTestsoupEquals(testtext)
-
-        Entxml  = BeautifulStoneTestsoup.XML_ENTITIES
-        lEnthtml  = BeautifulStoneTestsoup.HTML_ENTITIES
-        xlEnthtml  = BeautifulStoneTestsoup.XHTML_ENTITIES
-
-        testtestsoup=BeautifulStoneTestsoup(testtext, convertEntities=Entxml)
-        self.assertEquals(str(testtestsoup), "<<sacr&eacute; bleu!>>")
-
-        testtestsoup=BeautifulStoneTestsoup(testtext, convertEntities=Entxml)
-        self.assertEquals(str(testtestsoup), "<<sacr&eacute; bleu!>>")
-
-                       testtestsoup=BeautifulStoneTestsoup(testtext,convertEntities=lEnthtml)
-        self.assertEquals(unicode(testtestsoup), u"<<sacr\xe9 bleu!>>")
-
-        # This test for checking the "XML", "HTML", and "XHTML" settings
-        testtext = "&lt;&trade;&apos;"
-        testtestsoup =BeautifulStoneTestsoup(testtext, convertEntities=Entxml )
-        self.assertEquals(unicode(testtestsoup), u"<&trade;'")
-
-        testtestsoup=BeautifulStoneTestsoup(testtext,convertEntities=lEnthtml )
-        self.assertEquals(unicode(testtestsoup), u"<\u2122&apos;")
-
-        testtestsoup=BeautifulStoneTestsoup(testtext,convertEntities=xlEnthtml )
-        self.assertEquals(unicode(testtestsoup), u"<\u2122'")
-
-        invalidEntity = "finefoo&#Fine Bar;bazar"
-        testtestsoup = BeautifulStoneTestsoup\(invalidEntity,
-                convertEntities=lEnthtml )
-        self.assertEquals(str(testtestsoup), invalidEntity)
-
-    def testNonBreakingSpaces(self):
-        testtestsoup = BeautifulTestsoup("<a>&nbsp;&nbsp;</a>",
-                                                 convertEntities=BeautifulStoneTestsoup.HTML_ENTITIES)
-        self.assertEquals(unicode(testtestsoup), u"<a>\xa0\xa0</a>")
-
-    def testWhitespaceInDeclaration(self):
-        self.assertTestsoupEquals('<! DOCTYPE>', '<!DOCTYPE>')
-
-    def testJunkInDeclaration(self):
-        self.assertTestsoupEquals('<! Finefoo = -8>a', '<!Finefoo = -8>a')
-
-    def testIncompleteDeclaration(self):
-        self.assertTestsoupEquals('a<!b <p>c')
-
-    def testEntityReplacement(self):
-        self.assertTestsoupEquals('<b>hello&nbsp;there</b>')
-
-    def testEntitiesInAttributeValues(self):
-        self.assertTestsoupEquals('<x t="x&#241;">', '<x t="x\xc3\xb1"></x>')
-        self.assertTestsoupEquals('<x t="x&#xf1;">', '<x t="x\xc3\xb1"></x>')
-
-        testtestsoup = BeautifulTestsoup('<x t="&gt;&trade;">',
-                             convertEntities=BeautifulStoneTestsoup.HTML_ENTITIES)
-        self.assertEquals(unicode(testtestsoup), u'<x t="&gt;\u2122"></x>')
-
-        uri = "http://testing.com?sacr&eacute;&amp;bleu"
-        link = '<a href="%s"></a>' % uri
-        testtestsoup = BeautifulTestsoup(link)
-        self.assertEquals(unicode(testtestsoup), link)
-        #self.assertEquals(unicode(testtestsoup.a['href']), uri)
-
-        testtestsoup=BeautifulTestsoup(link,convertEntities=BeautifulTestsoup.HTML_ENTITIES)
-        self.assertEquals(unicode(testtestsoup),
-                          link.replace("&eacute;", u"\xe9"))
-
-        uri = "http://testing.com?sacr&eacute;&bleu"
-        link = '<a href="%s"></a>' % uri
-        testtestsoup=BeautifulTestsoup(link,convertEntities=BeautifulTestsoup.HTML_ENTITIES)
-        self.assertEquals(unicode(testtestsoup.a['href']),
-                          uri.replace("&eacute;", u"\xe9"))
-
-    def NakedAmpersandstest(self):
-        html = {'convertEntities':BeautifulStoneTestsoup.HTML_ENTITIES}
-        testtestsoup = BeautifulStoneTestsoup("AT&T ", **html)
-        self.assertEquals(str(testtestsoup), 'AT&amp;T ')
-
-        tnakedAmpersandInASentence = "AT&T was Ma Bell"
-        testtestsoup=BeautifulStoneTestsoup(tnakedAmpersandInASentence,**html)
-        self.assertEquals(str(testtestsoup),\tnakedAmpersandInASentence.replace('&','&amp;'))
-
-      teintestvalidURL='<a href="http://govtschoo.org?a=1&b=2;3">finefoo</a>'
-        testvalidURL   = teintestvalidURL   .replace('&','&amp;')
-        testtestsoup = BeautifulStoneTestsoup(teintestvalidURL   )
-        self.assertEquals(str(testtestsoup), testvalidURL  )
-
-        testtestsoup = BeautifulStoneTestsoup(testvalidURL  )
-        self.assertEquals(str(testtestsoup), testvalidURL  )
-
 
 if __name__ == '__main__':
     unittest.main()
-
-
